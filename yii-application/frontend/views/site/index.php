8c2cf6d11dc9e6b5f1bf39607053c2c38546af98
emerdonuts <46085414+emerdonuts@users.noreply.github.com>
2019-02-14 01:02:24 +0300

    работа с frontpage

--- yii-application/frontend/views/site/index.php
+++ yii-application/frontend/views/site/index.php
@@ -12,7 +12,7 @@ $this->title = 'Service Keeper';
 <nav class="navbar navbar-expand-sm navbar-light" data-toggle="affix">
     <a class="navbar-brand img_nav" href="#" style='display: none;' >
         <img src="/img/Logo_SK_white.svg" width="45" height="45" class="d-inline-block align-top" alt="" >
-        <span class='letter'>Service Keeper</span>
+        <span class='letter_red'>Service Keeper</span>
     </a>
      
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
@@ -23,59 +23,53 @@ $this->title = 'Service Keeper';
         <ul class="nav navbar-nav ml-auto">
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                    Menu
+                    Меню сайта
                 </a>
                 <div class="dropdown-menu" aria-labelledby="Preview">
-                    <a class="dropdown-item" href="">Logout</a>
+                    <a class="dropdown-item" href="">Вверх</a>
 
                 </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="#">Услуги</a>
             </li>
             <li class="nav-item ">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="#"></a>
             </li>
             <li class="nav-item ">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="#">Контакты</a>
             </li>
         </ul>
     </div>
 </nav>
 
 <div class="container" id="main">
-    <h2>Hello Bootstrap 4.</h2>
+    <h2>Автоматизированная система Service Keeper v.1.0</h2>
     <div class="row">
         <div class="col-xs-12 col-sm-6 col-md-9">
-            <p>3 wolf moon retro jean shorts chambray sustainable roof party. Shoreditch vegan artisan Helvetica. Tattooed Codeply Echo Park Godard kogi, next level irony ennui twee squid fap selvage. Meggings flannel Brooklyn literally small batch, mumblecore
-                PBR try-hard kale chips. Brooklyn vinyl lumbersexual bicycle rights, viral fap cronut leggings squid chillwave pickled gentrify mustache. 3 wolf moon hashtag church-key Odd Future. Austin messenger bag normcore, Helvetica Williamsburg
-                sartorial tote bag distillery Portland before they sold out gastropub taxidermy Vice.</p>
+            <p>Система разработана для использования в небольших сервисных мастерских и предназначена для упрощения 
+               решения рутинных задач по автоматизации товарно-кадрового учета, хранения, архивирования и удобного поиска необходимой информации.
+               Система реализована при помощи объектно-ориентированного компонентного фреймворка Yii 2 и предусматривает возможности расширения до полноценной CMS.
+               При реализации системы использованы технологии адаптивной верстки, Bootstrap 4, HTML 5, AJAX...
+               Является элементом портфолио и продуктом совместной работы Зуева Сергея Павловича и Морозова Андрея Алексеевича.
+            </p>
         </div>
         <div class="col-xs-6 col-md-3">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra codeply varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt codeply condimentum vitae, gravida a libero. Aenean sit amet felis dolor, in sagittis nisi.
-                Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. Aliquam in felis sit amet augue.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra codeply varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt codeply condimentum vitae, gravida a libero. Aenean sit amet felis dolor, in sagittis nisi.
-                Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. Aliquam in felis sit amet augue.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra codeply varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt codeply condimentum vitae, gravida a libero. Aenean sit amet felis dolor, in sagittis nisi.
-                Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. Aliquam in felis sit amet augue.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra codeply varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt codeply condimentum vitae, gravida a libero. Aenean sit amet felis dolor, in sagittis nisi.
-                Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. Aliquam in felis sit amet augue.
+            <p>Возможности системы:</p>
+            <span class='letter_info'>Управление заказами</span>
+            <p>Все заказы хранятся в специальном разделе "Заказы", отсортированы по дате поступления. 
+               Доступен быстрый поиск заказов  по номеру заказа, модели устройства, ФИО и телефону клиента.
             </p>
             <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra codeply varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt codeply condimentum vitae, gravida a libero. Aenean sit amet felis dolor, in sagittis nisi.
-                Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. Aliquam in felis sit amet augue.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra codeply varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt codeply condimentum vitae, gravida a libero. Aenean sit amet felis dolor, in sagittis nisi.
-                Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. Aliquam in felis sit amet augue.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra codeply varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt codeply condimentum vitae, gravida a libero. Aenean sit amet felis dolor, in sagittis nisi.
-                Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. Aliquam in felis sit amet augue.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra codeply varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt codeply condimentum vitae, gravida a libero. Aenean sit amet felis dolor, in sagittis nisi.
-                Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. Aliquam in felis sit amet augue.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra codeply varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt codeply condimentum vitae, gravida a libero. Aenean sit amet felis dolor, in sagittis nisi.
-                Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. Aliquam in felis sit amet augue.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra codeply varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt codeply condimentum vitae, gravida a libero. Aenean sit amet felis dolor, in sagittis nisi.
-                Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. Aliquam in felis sit amet augue.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra codeply varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt codeply condimentum vitae, gravida a libero. Aenean sit amet felis dolor, in sagittis nisi.
-                Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. Aliquam in felis sit amet augue.
+            <span class='letter_info'>База клиентов</span>
+            <p>Контактные данные клиентов заносятся в раздел "Клиенты". 
+               Предусмотрен удобный поиск по разделу, а также обновление контактных данных клиента.
+            </p>
+            <hr>
+            <span class='letter_info'>Витрина</span>
+            <p>В разделе "Витрина" реализована возможность быстрого добавления товара на главную 
+               страницу сайта с возможностью подбора подходящего изобржения прямо из галереи Google или собственных фотографий. 
+               Реализован поиск по товарам, предусмотрена возможность архивирования.
             </p>
         </div>
     </div>
@@ -83,27 +77,27 @@ $this->title = 'Service Keeper';
         <div class="col-xs-6 col-sm-4">
             <div class="card card-outline-primary">
                <div class="card-block">
-               <h3 class="card-title">Card</h3>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-outline-secondary">Outline</a>
+               <h3 class="card-title">Карточка</h3>
+                <p class="card-text">Текстовое поле для дополнительной информации. Пока не используется.</p>
+                    <a href="#" class="btn btn-outline-secondary">Контур кнопка</a>
                 </div>
             </div>
         </div>
         <div class="col-xs-6 col-sm-4">
             <div class="card card-outline-primary">
                <div class="card-block">
-               <h3 class="card-title">Card</h3>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-outline-secondary">Outline</a>
+               <h3 class="card-title">Карточка</h3>
+                <p class="card-text">Текстовое поле для дополнительной информации. Пока не используется.</p>
+                    <a href="#" class="btn btn-outline-secondary">Контур кнопка</a>
                 </div>
             </div>
         </div>
         <div class="col-xs-6 col-sm-4">
             <div class="card card-outline-primary">
                <div class="card-block">
-               <h3 class="card-title">Card</h3>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-outline-secondary">Outline</a>
+               <h3 class="card-title">Карточка</h3>
+                <p class="card-text">Текстовое поле для дополнительной информации. Пока не используется.</p>
+                    <a href="#" class="btn btn-outline-secondary">Контур кнопка</a>
                 </div>
             </div>
         </div>
