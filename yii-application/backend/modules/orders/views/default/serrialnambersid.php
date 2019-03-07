10bc1b7d87fb3b81ff6b6b7f64a0ea97b0641ca9
mtodzi <mtodzi@gmail.com>
2019-03-07 18:15:19 +0300

    правки

--- yii-application/backend/modules/orders/views/default/serrialnambersid.php
+++ yii-application/backend/modules/orders/views/default/serrialnambersid.php
@@ -3,23 +3,31 @@
     use backend\modules\orders\models\Brands;
     use backend\modules\orders\models\DeviceType;
     use backend\modules\orders\models\Devices;
+    use backend\modules\orders\models\SerialNumbers;
     
     if($model!=null){
-        if($model->serrialNambers->devise->brands->id_brands!=0 && $model->serrialNambers->devise->devicesType->id_device_type!=0 && $model->serrialNambers->devise->id_devices!=0){
+        if($model->serrialNambers->devise->brands->id_brands!=0 
+                && $model->serrialNambers->devise->devicesType->id_device_type!=0 
+                && $model->serrialNambers->devise->id_devices!=0
+                && $model->serrialNambers->id_serial_numbers!=0
+        ){
             $modelBrend = Brands::findOne($model->serrialNambers->devise->brands->id_brands);
             $modelDeviceTypeName = DeviceType::findOne($model->serrialNambers->devise->devicesType->id_device_type);
             $modelDevices = Devices::findOne($model->serrialNambers->devise->id_devices);
+            $modelSerialNumbers = SerialNumbers::findOne($model->serrialNambers->id_serial_numbers);;
             $modelOrders = $model;
         }else{
             $modelDeviceTypeName = null;
             $modelBrend = null;
             $modelDevices = null;
+            $modelSerialNumbers=null;
             $modelOrders = $model;
         }
     }else{
         $modelDeviceTypeName = null;
         $modelBrend = null;
         $modelDevices = null;
+        $modelSerialNumbers=null;
         $modelOrders = $model;
     }
 ?>
@@ -30,7 +38,10 @@
             'modelDeviceTypeName' => $modelDeviceTypeName,
             'modelDevices' => $modelDevices,
         ])
-    ?>    
+    ?>
+    <div id = "div_orders_serial_numbers_name-<?=(($modelOrders!=null)?$modelOrders->id_orders:0)?>">
+        <?=$this->render('serialnumbersnameindex', ['modelSerialNumbers' => $modelSerialNumbers,'modelOrders' =>$modelOrders])?>
+    </div>
 </div>    
 <?php $id = 'orders_hidden_serrial_nambers_id-'.(($model!=null)?$model->id_orders:0);?>
 <?=Html :: hiddenInput('OrdersEdit[serrial_nambers_id]', (($model!=null)?$model->serrial_nambers_id:0), ['id'=>$id])?>
